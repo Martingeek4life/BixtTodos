af9485a7fe4d4dac83c1348fe450f4afcf4c4202
MTPaulette <mayoguepaulette@gmail.com>
2021-10-03 19:54:54 +0100

    test du routage vue 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld'
 import todolist from '@/components/todo'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/todolist',
       name: 'todolist',
       component: todolist
+    },
+    {
+      path: '/HelloWorld',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
