25449887d52f1bd3633f45d2cd87b4a4f123c29f
chrisAfrotech <tchiaguiachristophe25@gmail.com>
2021-10-06 21:58:35 +0100

    Ajout du fichier workspace

---
+++ src/components/WorkspaceBixt/Workspace.vue
@@ -0,0 +1,22 @@
+<template>
+<section id="nav_bar">
+    <div class="menu esp">
+        Menu
+    </div>
+    <div class="logo esp">
+        logo
+    </div>
+        <button class="but_work esp">
+             <div class="text esp">
+                 workflow
+            </div>
+            <div class="icon esp">
+            </div>
+       </button>
+
+</section>
+</template>
+<style src="./workspace.css">
+</style>
+<script src = "./workspace.js">
+</script>
