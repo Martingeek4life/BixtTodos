9214d3d1d6658177779ea6b4b2384ac2ae7143ad
Tamo Mbobda <estadiotamo@gmail.com>
2021-10-06 07:38:42 +0100

    inscrition + logo

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import todolist from '@/components/todo'
-import Accueil from '@/components/accueil/Accueil'
 import Inscription from '@/components/inscription/Inscription'
 Vue.use(Router)
 
@@ -12,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'todolist',
       component: todolist
-    },
-    {
-      path: '/accueil',
-      name: 'accueil',
-      component: Accueil
     },
     {
       path: '/inscription',
