0977ee51f500b28fa793afe349daceaad5d77281
MTPaulette <mayoguepaulette@gmail.com>
2021-10-07 06:29:11 +0100

    ajout à la main des classes de kevin

---
+++ src/classes/Checklist.js
@@ -0,0 +1,32 @@
+import Utils from './Utils'
+
+class Task {
+  constructor (arg) {
+    let task = Object.assign({title: '', done: false}, Utils.initiateObjectFromArgument(arg))
+  }
+}
+
+export default class Checklist {
+  constructor (arg) {
+    let object = Object.assign({
+      title: '',
+      tasks:[]
+    }, Utils.initiateObjectFromArgument(arg))
+
+    for (let key in object) {
+      this[key] = object[key]
+    }
+  }
+
+  get tasksDone () {
+    return this.tasks.filter(el=>el.done)
+  }
+
+  get tasksUndone () {
+    return this.tasks.filter(el=>!el.done)
+  }
+
+  get progress () {
+    return parseInt(this.tasksDone.length * 100 / this.tasks.length)
+  }
+}
