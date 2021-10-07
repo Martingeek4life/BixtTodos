c6bd53bb245d681118fec45b8baf8dae1027b5e9
MTPaulette <mayoguepaulette@gmail.com>
2021-10-07 06:37:32 +0100

    ajout à la main des routes du fichier index.js de kevin

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import todolist from '@/components/todo'
 import Accueil from '@/components/accueil/Accueil'
+import ListTables from '@/components/ListTables/ListTables'
+import TableComponent from '@/components/Table/Table.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +18,18 @@ export default new Router({
       path: '/accueil',
       name: 'accueil',
       component: Accueil
+    },
+    {
+      path: '/workspace/:workspace_id/tables',
+      name: 'listTables',
+      component: ListTables,
+      props: true
+    },
+    {
+      path: '/workspace/:workspace_id/tables/:table_id',
+      name: 'table.show',
+      component: TableComponent,
+      props: true
     }
   ]
 })
